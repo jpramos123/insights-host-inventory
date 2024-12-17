f7ae8c31e1651bda0c5392f847d60bad8839a674
Joao Paulo Ramos <jramos@redhat.com>
2024-12-17 17:03:12 -0300

    feat: improve logging and add metrics

--- stale_host_notification.py
+++ stale_host_notification.py
@@ -32,7 +32,9 @@ from lib.metrics import stale_host_notification_processing_time
 LOGGER_NAME = "stale_host_notification"
 PROMETHEUS_JOB = "inventory-stale-host-notification"
 COLLECTED_METRICS = (
-    # add metric
+    stale_host_notification_count,
+    stale_host_notification_processing_time,
+    stale_host_notification_fail_count,
 )
 
 
@@ -127,7 +129,7 @@ def run(logger, session, notification_event_producer, shutdown_handler, applicat
         query = session.query(Host).filter(or_(False, *filter_stale_hosts))
         stale_hosts = query.all()
         if len(stale_hosts) > 0:
-            logger.info("%s hosts found as stale")
+            logger.info("%s hosts found as stale", len(stale_hosts))
             for host in stale_hosts:
                 identity = create_mock_identity_with_org_id(host.org_id)
                 result = _create_host_operation_result(host, identity, logger)
